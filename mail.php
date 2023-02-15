1ab1ac8daf8edb42706c983455e444b5828cbef3
Modest Co <jen@jenniferluchka.com>
2023-02-15 16:42:47 -0500

    updated styling

---
+++ mail.php
@@ -0,0 +1,14 @@
+
+<?php
+  if(isset( $_POST['name']))
+  $name = $_POST['name'];
+  if(isset( $_POST['email']))
+  $email = $_POST['email'];
+  if(isset( $_POST['message']))
+  $message = $_POST['message'];
+  if(isset( $_POST['subject']))
+  $subject = $_POST['subject'];
+
+  $content="From: $name \n Email: $email \n Message: $message";
+  $recipient = "jen@modestco.ca";
+?>
